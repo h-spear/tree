e05bccdb9642a3181f0b4ffa16be7fecfc57ff49
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-30 01:44:01 +0900

    add test data

---
+++ src/test/java/data/CardinalNumber.java
@@ -0,0 +1,14 @@
+package data;
+
+public class CardinalNumber {
+	public static String _10 = "Ten";
+	public static String _20 = "Twenty";
+	public static String _30 = "Thirty";
+	public static String _40 = "Forty";
+	public static String _50 = "Fifty";
+	public static String _60 = "Sixty";
+	public static String _70 = "Seventy";
+	public static String _80 = "Eighty";
+	public static String _90 = "Ninety";
+	public static String _100 = "One Hundred";
+}
